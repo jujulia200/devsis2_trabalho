8e8833a29ecff5f81c61721f9ba8d531eebba007
joao <joao.gpr@aluno.ifsc.edu.br>
2025-11-07 14:59:57 -0300

    ajuste no fornecedor

--- database/migrations/2025_10_28_192832_create_fornecedors_table.php
+++ database/migrations/2025_10_28_192832_create_fornecedors_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Produto;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,8 +14,16 @@ return new class extends Migration
     {
         Schema::create('fornecedors', function (Blueprint $table) {
             $table->id();
+            $table->string('nome',100);
+            $table->string('Cpf',16);
+            $table->string('email',50);
+            $table->string('telefone',20)->nullable();
+            $table->string('endereco',50);
+            $table->string('produto',100);
             $table->timestamps();
+
         });
+
     }
 
     /**
